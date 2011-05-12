ab0cc1eb5352bd30a6599d1cff3a8872f759c880
Paul Butcher <paul@paulbutcher.com>
2011-05-13 00:23:54 +0100

    Remove unnecessary return value from forward

--- src/main/scala/Turtle.scala
+++ src/main/scala/Turtle.scala
@@ -3,7 +3,7 @@ package com.example
 trait Turtle {
   def penUp()
   def penDown()
-  def forward(distance: Double): (Double, Double)
+  def forward(distance: Double)
   def turn(angle: Double)
   def getAngle: Double
   def getPosition: (Double, Double)
